26020b0f8aa9bb7c9ba483a72026bb12f5204997
JeffoLuis9 <jeffersonrc2508@gmail.com>
2025-05-14 04:55:06 -0500

    04:55

--- WebHooke/WebHookeModalidades/src/main/java/pe/edu/pucp/prog03/webhooke/modalidades/model/TipoSesion.java
+++ WebHooke/WebHookeModalidades/src/main/java/pe/edu/pucp/prog03/webhooke/modalidades/model/TipoSesion.java
@@ -12,4 +12,48 @@ public class TipoSesion {
     private int idModalidad;
     private double precio;
     private String tipo;
+
+    /**
+     * @return the idModalidad
+     */
+    public int getIdModalidad() {
+        return idModalidad;
+    }
+
+    /**
+     * @param idModalidad the idModalidad to set
+     */
+    public void setIdModalidad(int idModalidad) {
+        this.idModalidad = idModalidad;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
 }
